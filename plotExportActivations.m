dafc647fe04b565aaa73f1bde0d17b28aa4ab906
daliasen <18193042+daliasen@users.noreply.github.com>
2023-08-07 14:39:23 +0100

    BREAKING: remove an unnecessary input variable and tidy up
    - Get the number of signals from the activations matrix instead of as an input variable
    - Add a documentation for inputs
    - State the total number of signals in the titles of subplots
    - Tidy up the code

--- plotExportActivations.m
+++ plotExportActivations.m
@@ -1,30 +1,66 @@
-function plotExportActivations(H_updated, R, paramsSTFT, fs, flags, nameHP, paramsExport, expNum)
+function plotExportActivations(H_updated, ...
+                               paramsSTFT, ...
+                               fs, ...
+                               flags, ...
+                               nameHP, ...
+                               paramsExport, ...
+                               expNum)
+% Input:
+%   1) H_updated - A 2D matrix containing activations,
+%       number of sources x mixture length
+%   2) paramsSTFT - a structure with the following fields:
+%       w - window size that was used in the stft function (for 
+%           the time axis)
+%       overlap - percentage of window overlap that was used in 
+%           the stft function (for the time axis)
+%   3) fs - sampling frequency
+%   4) flags - a strcuture with the following fields:
+%           DispFigs - display figures flag
+%           ExportFigs - export figures, an array of flags: [pdf jpeg fig]
+%   5) nameHP - a char, 'P' for percussive, 'H' for harmonic
+%   6) paramsExport - a structure containing parameters for exporting files:
+%       t - timestamp
+%       folder - output folder
+%   7) expNum - experiment number
+    
     folder = paramsExport.folder;
     t = paramsExport.t;
     filePrefix = paramsExport.filePrefix;
 
     if flags.DispFigs==1 || max(flags.ExportFigs)==1 
-        if R > 6 
-            R = 6;
-            warning(['only the first ' R ' sources will be plotted'])
+        
+        [R, ~] = size(H_updated);
+        if R > 6
+            num_subplots = 6;
+            warning('only the first 6 signals will be plotted')
+        else
+            num_subplots = R;
         end
+        
         newFig(flags.DispFigs);
-        for r=1:R-1
-            subplot(R,1,r)
+        for r=1:num_subplots-1
+            subplot(num_subplots, 1, r)
             
-            plotTitle = ['Estimated Activations, Source ', num2str(r) ' (' nameHP ')'];
-            ax = activationsPlotStem(H_updated(r,:),paramsSTFT.w,paramsSTFT.overlap,fs,...
-                                     plotTitle);
+            plot_title = ['Estimated Activations (' nameHP ')'];
+            ax = activationsPlotStem(H_updated(r,:), ...
+                                     paramsSTFT.w, ...
+                                     paramsSTFT.overlap, ...
+                                     fs, ...
+                                     [plot_title ' (' num2str(r) ' out of ' num2str(R) ')']);
             
             ax.YLabel.String = '';
             ax.XLabel.String = '';
             ax.XTickLabel = '';
         end
-        subplot(R,1,r+1)
         
-        plotTitle = ['Estimated Activations, Source ', num2str(r+1) ' (' nameHP ')'];
-        activationsPlotStem(H_updated(r+1,:),paramsSTFT.w,paramsSTFT.overlap,fs,...
-                            plotTitle);
+        subplot(num_subplots, 1, r+1)
+        
+        plot_title = ['Estimated Activations (' nameHP ')'];
+        activationsPlotStem(H_updated(r+1,:), ...
+                            paramsSTFT.w, ...
+                            paramsSTFT.overlap, ...
+                            fs,...
+                            [plot_title ' (' num2str(r+1) ' out of ' num2str(R) ')']);
 
         h = gcf;
         
